c918f57292d448e4e60bfca5286545c02e4bae09
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-03-23 16:47:16 +0100

    added v-show function emailList.length == x

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
+      x: 10,
       emailList: [],
     }
   },
   mounted() {
 
-    for(let i = 0; i<10; i++){
+    for(let i = 0; i < this.x; i++){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then( (response) => {
            this.emailList.push(response.data.response);
